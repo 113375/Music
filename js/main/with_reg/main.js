d43b90a519a101bd8bd39d83c25a3dc760b0f6e2
Дима <knyazevdima05@gmail.com>
2021-05-23 18:46:29 +0300

    сделал поиск по песням

--- js/main/with_reg/main.js
+++ js/main/with_reg/main.js
@@ -6,38 +6,47 @@ let addAlbums = function () {
     request(data);
 
     function makePage(data) {
-        let div = document.querySelector(".albums");
-        data.forEach(album => {
+        let div = document.querySelector(".show-albums");
+        div.innerHTML = ""
+        if (data.length !== 0) {
+            data.forEach(album => {
+                let block = document.createElement("div");
+                block.classList.add("block-with-album");
+                block.albumId = album["album_id"];
+                block.albumName = album["name"]
+                block.genreId = album["genre_id"]
+                block.musicianId = album["musician_id"];
+
+                let name, musician, year, genre;
+                name = document.createElement("div");
+                name.innerText = album["name"];
+
+                musician = document.createElement("div");
+                musician.innerText = album["musician_name"];
+
+                year = document.createElement("div");
+                year.innerText = album["year"]
+
+                genre = document.createElement("div");
+                genre.innerText = album["genre_name"];
+
+
+                block.appendChild(name);
+                block.appendChild(genre)
+                block.appendChild(musician);
+                block.appendChild(year)
+
+                block.addEventListener("click", openAlbumPage)
+                div.appendChild(block)
+
+            })
+        } else {
             let block = document.createElement("div");
-            block.classList.add("block-with-album");
-            block.albumId = album["album_id"];
-            block.albumName = album["name"]
-            block.genreId = album["genre_id"]
-            block.musicianId = album["musician_id"];
-
-            let name, musician, year, genre;
-            name = document.createElement("div");
-            name.innerText = album["name"];
-
-            musician = document.createElement("div");
-            musician.innerText = album["musician_name"];
-
-            year = document.createElement("div");
-            year.innerText = album["year"]
-
-            genre = document.createElement("div");
-            genre.innerText = album["genre_name"];
-
-
-            block.appendChild(name);
-            block.appendChild(genre)
-            block.appendChild(musician);
-            block.appendChild(year)
-
-            block.addEventListener("click", openAlbumPage)
+            block.innerText = "Ничего не найдено";
+            block.style.marginLeft = "30px"
+            block.style.fontSize = "20px"
             div.appendChild(block)
-
-        })
+        }
     }
 
     function openAlbumPage() {
@@ -65,6 +74,40 @@ let addAlbums = function () {
             })
 
     }
+
+    let findAlbums = function () {
+        let find = document.querySelector(".find-albums");
+        find.addEventListener('keydown', function (e) {
+            if (e.keyCode === 13) {
+                albums(this.value);
+            }
+        });
+
+        function albums(value) {
+            let data = {
+                "query": `SELECT *
+                          FROM album a
+                                   LEFT JOIN genre g ON a.genre_id = g.id
+                                   LEFT JOIN musician m ON a.musician_id = m.id
+                          WHERE a.name LIKE "%${value}%"`, "all": true
+            }
+            request(data);
+        }
+
+        function resetButton() {
+            let button = document.querySelector(".button-album");
+            button.addEventListener("click", filterOff)
+        }
+
+        function filterOff() {
+            albums("");
+        }
+
+        resetButton();
+    }
+
+
+    findAlbums();
 }
 
 let addSongs = function () {
@@ -88,53 +131,97 @@ let addSongs = function () {
             })
             .then(function (data) {
                 makeSongs(data)
-                console.log(data)
             })
 
     }
 
     function makeSongs(data) {
-        let div = document.querySelector(".songs");
-        data.forEach(song => {
+
+        let div = document.querySelector(".show-songs");
+        div.innerHTML = ""
+        if (data.length !== 0) {
+            data.forEach(song => {
+                let block = document.createElement("div");
+                block.classList.add("block-with-song");
+                block.albumId = song["album_id"];
+                block.songName = song[1]
+                block.albumName = song["name"]
+                block.musicianId = song["musician_id"]
+                block.songId = song[0]
+                block.genreId = song["genre_id"]
+
+                let name, album, year, genre, musician;
+
+                name = document.createElement("div")
+                name.innerText = block.songName
+
+                album = document.createElement("div")
+                album.innerText = block.albumName
+
+                year = document.createElement("div")
+                year.innerText = song["year"];
+
+                musician = document.createElement("div");
+                musician.innerText = song["musician_name"]
+
+                block.appendChild(name);
+                block.appendChild(album)
+                block.appendChild(musician);
+                block.appendChild(year)
+
+                block.addEventListener("click", openSong)
+
+                div.appendChild(block)
+            })
+        } else {
             let block = document.createElement("div");
-            block.classList.add("block-with-song");
-            block.albumId = song["album_id"];
-            block.songName = song[1]
-            block.albumName = song["name"]
-            block.musicianId = song["musician_id"]
-            block.songId = song[0]
-            block.genreId = song["genre_id"]
-
-            let name, album, year, genre, musician;
-
-            name = document.createElement("div")
-            name.innerText = block.songName
-
-            album = document.createElement("div")
-            album.innerText = block.albumName
-
-            year = document.createElement("div")
-            year.innerText = song["year"];
-
-            musician = document.createElement("div");
-            musician.innerText = song["musician_name"]
-
-            block.appendChild(name);
-            block.appendChild(album)
-            block.appendChild(musician);
-            block.appendChild(year)
-
-            block.addEventListener("click", openAlbum)
-
+            block.innerText = "Ничего не найдено";
+            block.style.marginLeft = "30px"
+            block.style.fontSize = "20px"
             div.appendChild(block)
-        })
-        function openAlbum() {
+        }
+
+        function openSong() {
             window.open(`http://localhost:8888/Music/templates/album/album.php?albumId=
         ${this.albumId}&albumName=${this.albumName}&genreId=${this.genreId}
         &musicianId=${this.musicianId}&song=0`, '_blank');
         }
+
     }
 
+    let findSongs = function () {
+        let find = document.querySelector(".find-song");
+        find.addEventListener('keydown', function (e) {
+            if (e.keyCode === 13) {
+                songs(this.value);
+            }
+        });
+
+        function songs(value) {
+            let data = {
+                "query": `SELECT *
+                          FROM song s
+                                   LEFT JOIN album a ON s.album_id = a.album_id
+                                   LEFT JOIN musician m ON a.musician_id = m.id
+                          WHERE s.name LIKE "%${value}%"`, "all": true
+            }
+            request(data);
+        }
+
+        function resetButton() {
+            let button = document.querySelector(".button-song");
+            button.addEventListener("click", filterOff)
+        }
+
+        function filterOff() {
+            songs("");
+        }
+
+        resetButton();
+    }
+    findSongs();
 }
-addSongs()
+
+
+addSongs();
 addAlbums();
--- templates/main/with_reg/main.php
+++ templates/main/with_reg/main.php
@@ -1,15 +1,27 @@
 <div class="container-for-offers">
 
     <div class="albums-and-songs">
-            <div class="albums">
-                <div class="main-header">
-                    <input type="text" placeholder="Поиск по альбомам">
+        <div class="albums">
+            <div class="main-header">
+                <input type="text" class="find-albums" placeholder="Поиск по альбомам">
+                <div class="button button-album find-button">
+                    Сбросить
                 </div>
-<!--                тут будет половина с альбомами-->
             </div>
+            <div class="show-albums">
+
+                <!--                тут будет половина с альбомами-->
+            </div>
+        </div>
         <div class="songs">
             <div class="main-header">
-                <input type="text" placeholder="Поиск по песням">
+                <input type="text" class="find-song" placeholder="Поиск по песням">
+                <div class="button button-song find-button">
+                    Сбросить
+                </div>
+            </div>
+            <div class="show-songs">
+
             </div>
         </div>
     </div>
